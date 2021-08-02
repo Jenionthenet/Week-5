1415935effead1a13d8b125b3f4bf53d454193a9
Jen Ine <jbscott_5@yahoo.com>
2021-08-02 18:48:05 -0400

    part II is functioning

--- Day-1/Super-Heros/app.js
+++ Day-1/Super-Heros/app.js
@@ -4,25 +4,25 @@ const emptyDiv = document.getElementById("emptyDiv")
 const titleBtns = document.getElementsByClassName("movieBtns")
 let request = new XMLHttpRequest()
 
-request.addEventListener('load', function(){
+request.addEventListener('load', function () {
     console.log(this.responseText)
 
     const movies = JSON.parse(this.responseText)
     console.log(movies)
-    const movieItems = movies.Search.map(function(movie) {
+    const movieItems = movies.Search.map(function (movie) {
         const movieItem = `
         <li class = "lists">
             <div>
                 <img  class="posters" src="${movie.Poster}" alt="">
             </div>
-            <a href="#emptyDiv"><button onClick= showMovieDetails("${movie.imdbID}) class="movieBtns">${movie.Title}</button></a>
+            <h3 onClick= showMovieDetails("${movie.imdbID}") >${movie.Title}</h3>
         </li>
         `
         return movieItem
 
-    }) 
+    })
     moviesUL.innerHTML = movieItems.join("")
-    
+
 
 })
 request.open("GET", batMoviesURL)
@@ -30,36 +30,44 @@ request.send()
 
 
 function showMovieDetails(id) {
-    const movieId = btn.getAttribute('data-movieId')
+    //const movieId = button.getAttribute('movieBtns')
 
-        console.log(id) 
-}
-    // http://www.omdbapi.com/?i=insertSelectedimdbIDhere&apikey=insertyourkeyhere
+    console.log(id)
 
-    const selectedMovieUrl = `http://www.omdbapi.com/?i=${id}&apikey=47657be6
+// http://www.omdbapi.com/?i=insertSelectedimdbIDhere&apikey=insertyourkeyhere
+
+const selectedMovieUrl = `http://www.omdbapi.com/?i=${id}&apikey=47657be6
     `
 
-    console.log(selectedMovieUrl) // see if this url works 
-    let detalRequest = new XMLHttpRequest()
-    detalRequest.addEventListener("load", function(){
-        const result = JSON.parse(this.responseText)
-        console.log(result)
-    })
-        detailRequest.open("GET", selectedMovieUrl)
-        detailRequest.send()
+console.log(selectedMovieUrl) // see if this url works 
+
+let detailRequest = new XMLHttpRequest()
+detailRequest.addEventListener("load", function () {
+    const result = JSON.parse(this.responseText)
+    console.log(result)
+    const movieDetails = function() {
+        const  detailsItem = `
+        <h1>${result.Title}</h1>
+        <div><img class="posterPic"src="${result.Poster}" alt=""></div>
+        <h3>${result.year}</h3>
+        <h3>${result.Rated}</h3>
+        <h3>${result.Released}</h3>
+        <h3>${result.Director}</h3>`
+        
+        return detailsItem
+
+    }      
     
+    emptyDiv.innerHTML = movieDetails()
     
-    /*
-    movies.map(function(movie) {
-        const movieItemII = `<h1>${movie.Title}</h1>
-        <div><img class="posterPic"src="${movie.Poster}" alt=""></div>
-        <h3>${movie.year}</h3>
-        <h3>${movie.Rated}</h3>
-        <h3>${movie.Released}</h3>
-        <h3>${movie.Director}</h3>`
-        return movieItemII
-    }) 
 })
+
+
+
+detailRequest.open("GET", selectedMovieUrl)
+detailRequest.send()
+}
+
 
 
 
